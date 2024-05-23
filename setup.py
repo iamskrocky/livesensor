4bd6544a12372ed51d572c01e15e7687f3665fe9
iamskrocky <you@example.com>
2024-05-23 11:59:45 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements()->list[str]:
+
+    requirements_list = list[srt] = []
+
+    return requirements_list
+
+
+
+
+
+
+
+setup(
+name = 'sensor',
+version = "0.0.1",
+author = "prince",
+author_email= " iamskrocky@gmail.com",
+packages = find_packages(),
+
+install_reuires = get_requirements(),
+
+)
